ae6edb0225b1c2d7111e86505dac054922253a54
Nisharga Kabir <kabirnisharga@gmail.com>
2023-09-20 16:01:39 +0600

    footer

---
+++ src/Shared/Footer/Copyright.jsx
@@ -0,0 +1,12 @@
+
+const Copyright = () => {
+  return (
+    <footer className="footer footer-center p-4 text-base-content bg-black">
+        <div>
+            <p className="text-white">Copyright © Nisharga Kabir. All rights reserved.</p>
+        </div>
+    </footer>
+  )
+}
+
+export default Copyright
