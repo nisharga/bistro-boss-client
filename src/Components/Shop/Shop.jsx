342c369ca82ce7f2afa5217b85ed834f8567c5bc
Nisharga Kabir <kabirnisharga@gmail.com>
2023-09-21 11:06:45 +0600

    react-tabs setup

--- src/Components/Shop/Shop.jsx
+++ src/Components/Shop/Shop.jsx
@@ -1,7 +1,8 @@
 import { Helmet } from "react-helmet-async"
 import Cover from './../../Shared/Cover/Cover';
 import shopBannar from "../../assets/shop/banner2.jpg";
-
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import 'react-tabs/style/react-tabs.css'; 
 const Shop = () => {
   return (
     <div>
@@ -13,6 +14,19 @@ const Shop = () => {
             title="Our Shop" 
             subtitle="Would you like to try a dish"
         />
+        <div className="py-16 container mx-auto">
+         <Tabs defaultIndex={1} onSelect={(index) => console.log(index)}>
+            <TabList className={'text-center'}>
+                <Tab>SALAD</Tab>
+                <Tab>PIZZA</Tab>
+                <Tab>SOUPS</Tab>
+                <Tab>DESSERT</Tab>
+            </TabList>
+            <TabPanel>qqq</TabPanel>
+            <TabPanel>qqqqqq</TabPanel>
+            <TabPanel>qqqqaaaqq</TabPanel>
+         </Tabs>
+        </div>
     </div>
   )
 }
