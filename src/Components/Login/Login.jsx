4dbdf5eeb3c80a30fc26b9c7c11c5867e3d6f45f
Nisharga Kabir <kabirnisharga@gmail.com>
2023-09-22 15:05:50 +0600

    rechapcha for login page

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,10 +1,28 @@
+import { useEffect, useRef, useState } from 'react';
+import { loadCaptchaEnginge, LoadCanvasTemplate, LoadCanvasTemplateNoReload, validateCaptcha } from 'react-simple-captcha';
 
 const Login = () => {
+    const [disabled, setDisabled] = useState(true)
+    const captchaRef = useRef();
+    useEffect(() => {
+        loadCaptchaEnginge(6); 
+    }, [])
+
     const handleSubmit = (e) => {
         const email = e.target.email.value;
         const password = e.target.password.value;
         console.log(email, password);
         e.preventDefault();
+    }
+    const handleValidate = () => {
+        const user_captcha_value = captchaRef.current.value;
+        if (validateCaptcha(user_captcha_value)) {
+            setDisabled(false)
+        }
+   
+        else {
+            setDisabled(true)
+        }
     }
   return (
     <div>
@@ -23,13 +41,26 @@ const Login = () => {
                 <label className="label">
                     <span className="label-text">Password</span>
                 </label>
-                <input type="password"name="password" placeholder="password" className="input input-bordered" />
+                <input type="password"name="password" placeholder="password" className="input input-bordered" /> 
                 <label className="label">
                     <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                 </label>
                 </div>
+                <div className="form-control">
+                <label className="label">
+                    
+                    < LoadCanvasTemplate reloadColor="red" />
+                    
+                </label>
+                <input
+                ref={captchaRef} 
+                type="text"name="capcha" placeholder="Type The Capcha Above" className="input input-bordered" />
+                <button onClick={handleValidate} className="btn btn-outline btn-info mt-3">Validate</button>
+                </div>
                 <div className="form-control mt-6">
-                <button className="btn btn-primary">Login</button>
+                <button className="btn btn-primary"
+                    disabled={disabled}
+                >Login</button>
                 </div>
             </div>
             </form>
